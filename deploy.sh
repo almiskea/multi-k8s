6bbfc6ad009d04a55ce2e2c499c6feb2831e961f
almiskea <allawee2002@msn.com>
2018-09-24 11:51:28 -0700

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t almiskea/multi-client:latest -t almiskea/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t almiskea/multi-server:latest -t almiseka/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t almiskea/multi-worker:latest -t almiskea/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push almiskea/multi-client:latest
+docker push almiskea/multi-server:latest
+docker push almiskea/multi-worker:latest
+
+docker push almiskea/multi-client:$SHA
+docker push almiskea/multi-server:$SHA
+docker push almiskea/multi-worker:$SHA
+
+socker
+kubectl apply -f k8s
+kubectl set image deployment/server-deploymentcd server=almiskea/multi-server:$SHA
+kubectl set image deployment/client-deploymentcd client=almiskea/multi-client:$SHA
+kubectl set image deployment/worker-deploymentcd worker=almiskea/multi-worker:$SHA
