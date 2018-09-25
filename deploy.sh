578ae0429b4c4aa7da45a3644e40538339849aa5
almiskea <allawee2002@msn.com>
2018-09-25 10:33:42 -0700

    added deployment scripts9

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 docker build -t almiskea/multi-client:latest -t almiskea/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t almiskea/multi-server:latest -t almiseka/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t almiskea/multi-server:latest -t almiskea/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t almiskea/multi-worker:latest -t almiskea/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push almiskea/multi-client:latest
 docker push almiskea/multi-server:latest
 docker push almiskea/multi-worker:latest
+
 
 docker push almiskea/multi-client:$SHA
 docker push almiskea/multi-server:$SHA
